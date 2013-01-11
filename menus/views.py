c1c9dbb9a2385bc0ede66796b280aae9cbb3fde7
Jose Ayerdis <jose.ayerdis@booking.com>
2013-01-11 05:39:22 +0100

    Add support for DELETE method and json response for list

--- menus/views.py
+++ menus/views.py
@@ -1,18 +1,26 @@
 from flask import Flask, request, session, redirect, url_for, abort, render_template, flash
-from flask import Blueprint
+from flask import Blueprint,jsonify, make_response, Response
 from kitch_db import db
+
 
 app = Blueprint('menus',__name__,template_folder='templates')
 
 
-@app.route('/menu/',methods=['GET'])
+@app.route('/menus/',methods=['GET'])
 def list():
+    
+    json_mime = request.accept_mimetypes.best_match(['application/json','text/html'])
+    
     cur = db.execute('select title from menus')
     menus = [dict(title=row[0]) for row in cur.fetchall()]
-    return render_template('show_menus.html', menus=menus)
+
+    if json_mime=='application/json':
+        return jsonify(items=menus)
+    elif json_mime=='text/html':
+        return render_template('show_menus.html', menus=menus)
     
 
-@app.route('/menu/',methods=['POST'])
+@app.route('/menus/',methods=['POST'])
 def update():
     if not session.get('logged_in'):
         abort(401)
@@ -22,6 +30,15 @@ def update():
 
     return redirect(url_for('menus.list'))
 
-@app.route('/menu/',methods=['DELETE'])
-def delete():
-    return 'DElete Menu'
+@app.route('/menus/<string:id>',methods=['DELETE'])
+def delete(id):
+    rowcount = db.executemany('delete from menus where id=?', id).rowcount
+    
+    if rowcount != 0:
+        return Response(status= 200)
+    
+    response = { 'message':'Delete succesfully'}
+    return make_response(jsonify(response), 202)
+
+    
+    
