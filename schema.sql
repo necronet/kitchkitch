4dcde25d89513eb1ea4901997b511c5d55bb01cd
Jose Ayerdis <jose.ayerdis@booking.com>
2013-01-12 19:52:38 +0100

    Schema to add default user admin

--- schema.sql
+++ schema.sql
@@ -1,8 +1,10 @@
-drop table if exist users;
+drop table if exists users;
 create table users( user_id string primary key, 
 username string not null, 
 password string not null,
 active int default 0);
+
+insert into users values('21232f297a57a5a743894a0e4a801fc3','admin','admin',1);
 
 drop table if exists menus;
 create table menus (
