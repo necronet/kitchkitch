130ffe5b1d96d37586053ab0f55c13dfc0fbf68d
Jose Ayerdis <jose.ayerdis@booking.com>
2013-01-12 07:09:17 +0100

    Remove unnecesary args parsing to script

--- kitch_db.py
+++ kitch_db.py
@@ -1,5 +1,6 @@
 import sqlite3
 from flask import _app_ctx_stack
+from contextlib import closing
 from werkzeug.local import LocalProxy
 
 def init_app(app):
@@ -12,12 +13,6 @@ def get_db():
 		con = sqlite3.connect(ctx.app.config['DATABASE'])
 		ctx.kitch_db=con
 	return con
-
-def init_db():
-    with closing(get_db()) as db:
-        with app.open_resource('schema.sql') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
 
 def close_database_connection(exception):
 	con = getattr(_app_ctx_stack, 'kitch_db',None)
--- schema.sql
+++ schema.sql
@@ -1,3 +1,9 @@
+drop table if exist users;
+create table users( user_id string primary key, 
+username string not null, 
+password string not null,
+active int default 0);
+
 drop table if exists menus;
 create table menus (
   id integer primary key autoincrement,
