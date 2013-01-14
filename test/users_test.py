9f1c710148942dc8c4ff3ef6a0d8decc90003430
Jose Ayerdis <jose.ayerdis@booking.com>
2013-01-14 09:12:16 +0100

    Create BaseTest class to improve testing for user and general request calls.

--- test/users_test.py
+++ test/users_test.py
@@ -5,24 +5,34 @@ parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0,parentdir) 
 from runserver import app
 
-class GeneralTest(unittest.TestCase):
+class BaseTest(unittest.TestCase):
 
 	def setUp(self):
-		pass
+		self.c= app.test_client()
 
 	def tearDown(self):
 		pass
 
+class GeneralTest(BaseTest):
+
 	def test_empty_data_post(self):
-		with app.test_client() as c:
-			rv = c.post('/login/')
-			assert '400' in rv.status
+		rv = self.c.post('/menus/',data=json.dumps({}),content_type='application/json')
+		assert '400' in rv.status
 
 	def test_wrong_data_post(self):
-		with app.test_client() as c:
-			rv = c.post('/login/', data=json.dumps({"username":"admin","password":"password"}),content_type='application/json')
-			assert '400' in rv.status
+		rv = self.c.post('/menus/', data=json.dumps({"randonm":"items","goes":"here"}),content_type='application/json')
+		assert '400' in rv.status
 
+	def test_random_resource(self):
+		rv = self.c.post('/random_non_existing_resource/')
+		assert '404' in rv.status
+
+
+class UsersTest(BaseTest):
+	def test_login_wrong_pasword(self):
+		rv=self.c.post('/login/',data=json.dumps({"username":"admin","password":"wonrg_password_goes"}),content_type='application/json')
+		print rv.data
+		assert rv is not None
 
 if __name__ == '__main__':
     unittest.main()
