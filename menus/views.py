6e7f3154217ea6d1672bfd51b0a77089aa3f9702
Jose Ayerdis <jose.ayerdis@booking.com>
2013-01-19 08:51:03 +0100

    Change to MethodView clas for standarize API

--- menus/views.py
+++ menus/views.py
@@ -1,56 +1,64 @@
 from flask import request, render_template,Blueprint,jsonify, make_response, Response
+from flask.views import MethodView
 from kitch_db import db
 from flask.ext.login import login_required
 
+class MenuService(MethodView):
+    
+    #@login_required
+    def get(self, menu_id):
+    
+        json_mime = request.accept_mimetypes.best_match(['application/json','text/html'])
+        
+        cur = db.execute('select title from menus')
+        menus = [dict(title=row[0]) for row in cur.fetchall()]
+
+        if json_mime=='application/json':
+            return jsonify(items=menus)
+        elif json_mime=='text/html':
+            return render_template('show_menus.html', menus=menus)
+
+    
+    #@login_required
+    def post(self):
+        
+        for json_object in request.json['items']:
+            db.execute('insert into menus(title) values(?) ', [json_object['title']])
+            response=make_response(jsonify({'message':'Inserted succesfully'}),201,{'Location':request.url})
+
+        db.commit()
+
+        return response
+
+    
+    def put(self, menu_id):
+        for json_object in request.json['items']:
+            if json_object.has_key('id'):
+                db.execute('update menus set title=? where id=?',  [json_object['title'],json_object['id']])
+                
+        db.commit()
+
+        return make_response(jsonify({'message':'Succesfully updated'}))
+
+
+    
+    @login_required
+    def delete(self, menu_id):
+        rowcount = db.executemany('delete from menus where id=?', menu_id).rowcount
+        
+        if rowcount != 0:
+            return Response(status= 200)
+        
+        response = { 'message':'Delete succesfully'}
+        return make_response(jsonify(response), 202)
+
+
+
 app = Blueprint('menus',__name__,template_folder='templates')
 
-@app.route('/menus/',methods=['GET'])
-@login_required
-def list():
-    
-    json_mime = request.accept_mimetypes.best_match(['application/json','text/html'])
-    
-    cur = db.execute('select title from menus')
-    menus = [dict(title=row[0]) for row in cur.fetchall()]
-
-    if json_mime=='application/json':
-        return jsonify(items=menus)
-    elif json_mime=='text/html':
-        return render_template('show_menus.html', menus=menus)
-    
-
-@app.route('/menus/',methods=['POST'])
-#@login_required
-def post():
-    
-    for json_object in request.json['items']:
-        db.execute('insert into menus(title) values(?) ', [json_object['title']])
-        response=make_response(jsonify({'message':'Inserted succesfully'}),201,{'Location':request.url})
-
-    db.commit()
-
-    return response
-
-@app.route('/menus/',methods=['PUT'])
-def put():
-    for json_object in request.json['items']:
-        if json_object.has_key('id'):
-            db.execute('update menus set title=? where id=?',  [json_object['title'],json_object['id']])
-            
-    db.commit()
-
-    return make_response(jsonify({'message':'Succesfully updated'}))
-
-@app.route('/menus/<string:id>',methods=['DELETE'])
-@login_required
-def delete(id):
-    rowcount = db.executemany('delete from menus where id=?', id).rowcount
-    
-    if rowcount != 0:
-        return Response(status= 200)
-    
-    response = { 'message':'Delete succesfully'}
-    return make_response(jsonify(response), 202)
-
-    
-    
+user_view = MenuService.as_view('menu_service')
+app.add_url_rule('/menus/', defaults={'menu_id': None},
+                 view_func=user_view, methods=['GET',])
+app.add_url_rule('/menus/', view_func=user_view, methods=['POST',])
+app.add_url_rule('/menus/<int:menu_id>', view_func=user_view,
+                 methods=['GET', 'PUT', 'DELETE'])
