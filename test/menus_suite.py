2736ff92050925e66772fec5974cc9fb20d27d07
Jose Ayerdis <jose.ayerdis@booking.com>
2013-01-20 17:52:54 +0100

    Assert 202 for test delete and fix DELETE test method for menuItems resources

--- test/menus_suite.py
+++ test/menus_suite.py
@@ -48,6 +48,7 @@ class MenuTest(BaseTest):
 
 		for data in items['items']:
 			rv=self.c.delete('/menus/%s' % data['uid'],content_type='application/json')
+			assert rv.status_code==202
 		
 		rv=self.c.get('/menus/',headers=[('Accept','application/json'),('Authorization',self.token)])
 		assert rv.status_code == 200
@@ -55,7 +56,7 @@ class MenuTest(BaseTest):
 		
 		assert len(items['items']) == 0
 
-class MenuItemService(BaseTest):
+class MenuItemTest(BaseTest):
 	def test_list_menu_items(self):
 		rv=self.c.get('/menuItems/',headers=[('Accept','application/json')])		
 		
@@ -70,33 +71,38 @@ class MenuItemService(BaseTest):
 		rv=self.c.get('/menus/',headers=[('Accept','application/json')])
 		
 		uid=json.loads(rv.data)['items'][0]['uid']		
-
+		
 		menu_items={"items":[{"title":"Menu Items #1",'description':'delicous meal to serve','price':10.25}]}
 
 		rv=self.c.post('/menuItems/?menus_uid=%s'%uid,data=json.dumps(menu_items),content_type='application/json')
 		assert rv.status_code == 201
 
 	def test_put(self):
-		rv=self.c.get('/menuItems/',headers=[('Accept','application/json')])
+		rv=self.c.get('/menus/',headers=[('Accept','application/json')])
+		menus_uid=json.loads(rv.data)['items'][0]['uid']
+		
+		rv=self.c.get('/menuItems/?menus_uid=%s' % menus_uid,headers=[('Accept','application/json')])
+		
 		menu_item=json.loads(rv.data)['items'][0]
 		update_data = {'items':[{'uid':menu_item['uid'],'title':menu_item['title'],'description':menu_item['description'],'price':menu_item['price']}]}
 		
-		rv=self.c.put('/menuItems/',data=json.dumps(update_data),content_type='application/json')		
-		print rv.data
+		rv=self.c.put('/menuItems/?menus_uid=%s' % menus_uid ,data=json.dumps(update_data),content_type='application/json')		
+		assert rv.status_code == 200
 
 	def test_delete_items(self):
 		rv=self.c.get('/menus/',headers=[('Accept','application/json')])
 		assert rv.status_code == 200
-		items=json.loads(rv.data)
-		
 		items=json.loads(rv.data)['items']
 		menus_uid=items[0]['uid']
-		rv=self.c.get('/menuItems/menus_uid=%s'%menus_uid,headers=[('Accept','application/json')])
+		
+		rv=self.c.get('/menuItems/?menus_uid=%s'% menus_uid ,headers=[('Accept','application/json')])
 
 		for data in json.loads(rv.data)['items']:
-			rv=self.c.delete('/menusItems/%s' % data['uid'],content_type='application/json')
+
+			rv=self.c.delete('/menuItems/%s?menus_uid=%s' % (data['uid'],menus_uid),content_type='application/json')
+			assert rv.status_code == 202
 		
-		rv=self.c.get('/menuItems/menus_uid=%s'%menus_uid,headers=[('Accept','application/json')])
+		rv=self.c.get('/menuItems/?menus_uid=%s'%menus_uid,headers=[('Accept','application/json')])
 
 		assert rv.status_code == 200
 		items=json.loads(rv.data)
