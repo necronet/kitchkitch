fc9bda2282752f830474f694dd23e7eed1ce3287
Jose Ayerdis <jose.ayerdis@booking.com>
2013-01-27 07:45:28 +0100

    Change MEnus views query to support torndb and MySQL

--- kitch/menus/views.py
+++ kitch/menus/views.py
@@ -14,14 +14,15 @@ class MenuService(BaseService):
         json_mime = request.accept_mimetypes.best_match(['application/json','text/html'])
         
         if menu_uid is None:
-            cur = db.execute('select uid,title from menus where active=1 limit ? offset ?' ,(self.limit, self.offset) )
-            items = [dict(uid=row[0],title=row[1]) for row in cur.fetchall()]
+
+            rows = db.query("select uid,title from menus where active=1 limit %s offset %s" ,self.limit, self.offset )
+            items = [dict(uid=row.uid,title=row.title) for row in rows]
 
             response=jsonify(items=items)
         else:
-            cur = db.execute('select uid,title from menus where uid=? and active=1 limit ? offset ?' , (menu_uid,self.limit, self.offset) )
-            result=cur.fetchone()
-            item = dict(uid=result[0],title=result[1])
+            result = db.get("select uid,title from menus where uid=%s and active=1 limit %s offset %s" ,menu_uid,self.limit, self.offset) 
+            
+            item = dict(uid=result.uid,title=result.title)
             response=jsonify(item)
             
 
@@ -36,10 +37,10 @@ class MenuService(BaseService):
         for json_object in request.json['items']:
             menu= KitchObject(json_object)
             uid =str(uuid.uuid1())
-            db.execute('insert into menus(uid,title) values(?,?) ', [uid,menu.title])
+            db.execute('insert into menus(uid,title) values(%s,%s) ', uid,menu.title)
             response=make_response(jsonify({'message':'Inserted succesfully'}),201,{'Location':request.url})
 
-        db.commit()
+        
 
         return response
 
@@ -48,17 +49,14 @@ class MenuService(BaseService):
         for json_object in request.json['items']:
             menu= KitchObject(json_object)
             if json_object.has_key('uid'):
-                db.execute('update menus set title=? where uid=?',  [menu.title,menu.uid])
-                
-        db.commit()
+                db.execute('update menus set title=%s where uid=%s',  menu.title,menu.uid)
 
         return make_response(jsonify({'message':'Succesfully updated'}))
 
     @login_required
     def delete(self, menu_uid):
 
-        rowcount = db.execute('update menus set active=0 where uid=?', (menu_uid,)).rowcount
-        db.commit()
+        rowcount = db.execute_rowcount('update menus set active=0 where uid=%s', menu_uid)
         
         if rowcount == 0:
             return Response(status= 200)
@@ -74,16 +72,16 @@ class MenuItemsService(BaseService):
         json_mime = request.accept_mimetypes.best_match(['application/json'])
 
         if item_uid is not None:
-            cur = db.execute('select uid,title,description,price from items where uid=? and active=1 limit ? offset ?',(item_uid,self.limit, self.offset))
-            result=cur.fetchone()
-            item = dict(uid=result[0],title=result[1],description=result[2],price=result[3])
+            result = db.get('select uid,title,description,price from items where uid=%s and active=1 limit %s offset %s',item_uid,self.limit, self.offset)
+
+            item = dict(uid=result.uid,title=result.title,description=result.description,price=str(result.price))
             return jsonify(item)
         
         if menus_uid is None:
-            cur = db.execute('select uid,title,description,price from items where active=1 limit %d offset %d' % (self.limit, self.offset))
+            result = db.query("select uid,title,description,price from items where active=1 limit %s offset %s", self.limit, self.offset)
         else:
-            cur = db.execute('select uid,title,description,price from items i inner join menus_items mi on mi.items_uid=i.uid where mi.active=1 and mi.menus_uid=? limit ? offset ?' ,[menus_uid,self.limit, self.offset,])
-        menus_items = [dict(uid=row[0],title=row[1],description=row[2],price=row[3]) for row in cur.fetchall()]
+            result = db.query("select uid,title,description,price from items i inner join menus_items mi on mi.items_uid=i.uid where mi.active=1 and mi.menus_uid=%s limit %s offset %s" ,menus_uid,self.limit, self.offset)
+        menus_items = [dict(uid=row.uid,title=row.title,description=row.description,price=str(row.price)) for row in result]
         
         if json_mime=='application/json':
             return jsonify(items=menus_items)
@@ -98,13 +96,13 @@ class MenuItemsService(BaseService):
             menu_items= KitchObject(json_object)
             uid =str(uuid.uuid1())
 
-            db.execute('insert into items(uid,title,description,price) values(?,?,?,?) ', [uid,menu_items.title,menu_items.description,menu_items.price])
-            db.execute('insert into menus_items(menus_uid,items_uid) values(?,?) ', [menus_uid,uid])
+            db.execute('insert into items(uid,title,description,price) values(%s,%s,%s,%s) ', uid,menu_items.title,menu_items.description,menu_items.price)
+            db.execute('insert into menus_items(menus_uid,items_uid) values(%s,%s) ', menus_uid,uid)
 
         
         response=make_response(jsonify({'message':'Inserted succesfully'}),201,{'Location':request.url})
 
-        db.commit()
+        
 
         return response
 
@@ -118,10 +116,8 @@ class MenuItemsService(BaseService):
         for json_object in request.json['items']:            
 
             menu_items= KitchObject(json_object)
-            db.execute('update items set title=?, description=?, price=? where uid=?',  [menu_items.title,menu_items.description,menu_items.price,menu_items.uid])
-            db.execute('update menus_items set menus_uid=? where items_uid=?',  [menus_uid,menu_items.uid])
-                
-        db.commit()
+            db.execute("update items set title=%s, description=%s, price=%s where uid=%s",  menu_items.title,menu_items.description,menu_items.price,menu_items.uid)
+            db.execute("update menus_items set menus_uid=%s where items_uid=%s",menus_uid,menu_items.uid)
 
         return make_response(jsonify({'message':'Succesfully updated'}))
     def delete(self,item_uid):
@@ -131,9 +127,9 @@ class MenuItemsService(BaseService):
         if menus_uid is None:
             abort(400, 'Missing menus_uid parameter. Not allowed to delete items without a menu to be referenced')
         
-        rowcount = db.execute('update menus_items set active=0 where menus_uid=? and items_uid=?', (menus_uid,item_uid)).rowcount
+        rowcount = db.execute_rowcount('update menus_items set active=0 where menus_uid=%s and items_uid=%s', menus_uid,item_uid)
         
-        db.commit()
+        
         
         if rowcount == 0:
             return Response(status= 200)
