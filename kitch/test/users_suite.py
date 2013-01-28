d2029bade2cd3050fc41d6722bbba81030c21d25
Jose Ayerdis <jose.ayerdis@booking.com>
2013-01-28 03:03:22 +0100

    Inherit from BaseTest to TestCase the user tests

--- kitch/test/users_suite.py
+++ kitch/test/users_suite.py
@@ -1,17 +1,23 @@
-from base import BaseTest, login
+import sys
+import os
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0,parentdir) 
+from runserver import app
 import unittest
+from base import login
 
-class UsersTest(BaseTest):
+class UsersTest(unittest.TestCase):
+
+	def setUp(self):
+		self.c= app.test_client()
+		app.config['TESTING'] = True
+
 	def test_login_wrong_pasword(self):
 		
 		rv=login(self.c,"admin","wrongpassword")
 			
 		#assert rv.headers['Location'] in '/login/'
 		assert rv.status_code == 401
-
-	@unittest.skip('Not valid for users module test')
-	def get(self):
-		pass
 
 	def test_login_right_password(self):
 		
