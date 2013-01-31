998b30f87cb60a6366663305ae0383e6c383f4ec
Jose Ayerdis <jose.ayerdis@booking.com>
2013-01-31 15:00:40 +0100

    HTTP response standarize
    Make get_response and post_response method in base to standarize
    the responses across the services, so all calls inherit from BaseService
    retrieve same HTTP Response structure. GET, POST, PUT and DELETE

--- kitch/menus/views.py
+++ kitch/menus/views.py
@@ -1,6 +1,6 @@
 import uuid
 from utils.entities import KitchObject, BaseService
-from flask import request, render_template,Blueprint,jsonify, make_response, Response, url_for
+from flask import request, Blueprint,jsonify, make_response, Response, url_for
 from utils.exceptions import abort
 from kitch_db import db
 from flask.ext.login import login_required
@@ -15,7 +15,6 @@ class MenuService(BaseService):
     def get(self, uid):
         super(MenuService, self).get(uid)
 
-        json_mime = request.accept_mimetypes.best_match(['application/json','text/html'])
         menu_items=[]
         if uid is None:
 
@@ -33,10 +32,7 @@ class MenuService(BaseService):
                     menu_items=dict(href='%s?menus_uid=%s' % ( url_for('.menuItemService',_method='GET',_external=True),row.uid,))
 
                 items.append( dict(href='%s%s'%(request.base_url,row.uid),uid=row.uid,title=row.title,items=menu_items) )
-
-            response=jsonify(items=items)
         else:
-
             result = db.get("select uid,title from menus where uid=%s and active=1 limit %s offset %s" ,uid,self.limit, self.offset)
 
             if self.expand is not None:
@@ -48,14 +44,16 @@ class MenuService(BaseService):
             if not menu_items:
                 menu_items=dict(href='%s?menus_uid=%s' % ( url_for('.menuItemService',_method='GET',_external=True),result.uid,) )
 
-            item = dict(href="%s" % (request.base_url,),uid=result.uid,title=result.title, items=menu_items)
+            items = dict(href="%s" % (request.base_url,),uid=result.uid,title=result.title, items=menu_items)
 
-            response=jsonify(item)
 
-        if json_mime=='application/json':
+        return self.get_response(items)
+
+        """ if json_mime=='application/json':
             return response
         elif json_mime=='text/html':
             return render_template('show_menus.html', menus=items)
+        """
 
     def fetch_items(self,uid):
         #print "select uid,title,description,price from items i inner join menus_items mi on mi.items_uid=i.uid where mi.active=1 and mi.menus_uid=%s" % uid
@@ -71,11 +69,10 @@ class MenuService(BaseService):
             uid =str(uuid.uuid1())
             #TODO: add location to each item
             db.execute('insert into menus(uid,title) values(%s,%s) ', uid,menu.title)
-            response=make_response(jsonify({'message':'Inserted succesfully'}),201,{'Location':request.url})
+            
             db.commit()
         
-
-        return response
+        return self.post_response()
 
     @login_required
     def put(self):
@@ -85,60 +82,49 @@ class MenuService(BaseService):
                 db.execute('update menus set title=%s where uid=%s',  menu.title,menu.uid)
                 db.commit()
 
-        return make_response(jsonify({'message':'Succesfully updated'}))
+        return self.put_response()
 
     @login_required
     def delete(self, uid):
-
-        rowcount = db.execute_rowcount('update menus set active=0 where uid=%s', uid)
+        db.execute_rowcount('update menus set active=0 where uid=%s', uid)
         db.commit()
-        if rowcount == 0:
-            return Response(status= 200)
-        
-        response = { 'message':'Delete succesfully'}
-        return make_response(jsonify(response), 202)
+        return self.delete_response()
 
 class MenuItemsService(BaseService):
     def get(self, uid):
         super(MenuItemsService, self).get(uid)
         menus_uid=request.args.get('menus_uid')
-        
-        json_mime = request.accept_mimetypes.best_match(['application/json'])
 
         if uid is not None:
             result = db.get('select uid,title,description,price from items where uid=%s and active=1 limit %s offset %s',uid,self.limit, self.offset)
+            items = dict(href='%s%s'%(request.base_url,uid), uid=result.uid,title=result.title,description=result.description,price=str(result.price))
+            return self.get_response(items)
 
-            item = dict(href='%s%s'%(request.base_url,uid), uid=result.uid,title=result.title,description=result.description,price=str(result.price))
-            return jsonify(item)
-        
         if menus_uid is None:
             result = db.query("select uid,title,description,price from items where active=1 limit %s offset %s", self.limit, self.offset)
         else:
             result = db.query("select uid,title,description,price from items i inner join menus_items mi on mi.items_uid=i.uid where mi.active=1 and mi.menus_uid=%s limit %s offset %s" ,menus_uid,self.limit, self.offset)
-        menus_items = [dict(href='%s%s'%(request.base_url,row.uid),uid=row.uid,title=row.title,description=row.description,price=str(row.price)) for row in result]
         
-        if json_mime=='application/json':
-            return jsonify(items=menus_items)
+        items = [dict(href='%s%s'%(request.base_url,row.uid),uid=row.uid,title=row.title,description=row.description,price=str(row.price)) for row in result]
+        
+        return self.get_response(items)
+        
         
     def post(self):
         menus_uid=request.args.get('menus_uid')
         if menus_uid is None:
             abort(400, 'Missing menus_uid parameter. Not allowed to create items without a menu to be referenced')
 
-
+        
         for json_object in request.json['items']:
-            menu_items= KitchObject(json_object)
+            menu_items=KitchObject(json_object)            
             uid =str(uuid.uuid1())
 
             db.execute('insert into items(uid,title,description,price) values(%s,%s,%s,%s) ', uid,menu_items.title,menu_items.description,menu_items.price)
             db.execute('insert into menus_items(menus_uid,items_uid) values(%s,%s) ', menus_uid,uid)
-            db.commit()
+            db.commit()        
         
-        response=make_response(jsonify({'message':'Inserted succesfully'}),201,{'Location':request.url})
-
-        
-
-        return response
+        return self.post_response()
 
     def put(self):
 
@@ -154,7 +140,7 @@ class MenuItemsService(BaseService):
             db.execute("update menus_items set menus_uid=%s where items_uid=%s",menus_uid,menu_items.uid)
             db.commit()
 
-        return make_response(jsonify({'message':'Succesfully updated'}))
+        return self.put_response()
     def delete(self,uid):
 
         menus_uid=request.args.get('menus_uid')
@@ -162,15 +148,11 @@ class MenuItemsService(BaseService):
         if menus_uid is None:
             abort(400, 'Missing menus_uid parameter. Not allowed to delete items without a menu to be referenced')
         
-        rowcount = db.execute_rowcount('update menus_items set active=0 where menus_uid=%s and items_uid=%s', menus_uid,uid)
+        db.execute_rowcount('update menus_items set active=0 where menus_uid=%s and items_uid=%s', menus_uid,uid)
         db.commit()
         
         
-        if rowcount == 0:
-            return Response(status= 200)
-        
-        response = { 'message':'Delete succesfully'}
-        return make_response(jsonify(response), 202)
+        return self.delete_response()
 
 app = Blueprint('menus',__name__,template_folder='templates')
 
@@ -189,7 +171,6 @@ def register_api(view, endpoint, url, pk, pk_type='string'):
     app.add_url_rule(url, view_func=view_func, methods=['POST','PUT',])
     app.add_url_rule('%s<%s:%s>' %(url,pk_type,pk), view_func=view_func,
                      methods=['GET', 'DELETE'])
-
 
 
 register_api(MenuService, 'menuService','/menus/','uid')
--- kitch/utils/entities.py
+++ kitch/utils/entities.py
@@ -1,8 +1,9 @@
 from flask.views import MethodView
-from flask import request
+from flask import request, jsonify, make_response
+
 
 class KitchObject(object):
-    def __init__(self, obj):
+    def __init__(self, obj):        
         for k, v in obj.iteritems():
             if isinstance(v, dict):
                 setattr(self, k, KitchObject(v))
@@ -16,13 +17,25 @@ class BaseService(MethodView):
         self.limit= int(self.get_parameter('limit',50))
         self.expand=request.args.get('expand')
 
-
-
     def get_parameter(self, param_name, default_value=0):
         parameter=request.args.get(param_name)
-
         return default_value if parameter is None or type(parameter)!=type(default_value) else parameter
 
+    def get_response(self, datas):
+        
+        if type(datas)==list:
+            return jsonify(items=datas)    
+        return jsonify(datas)
+
+    def post_response(self):        
+        return make_response(jsonify({'message':'Create succesfully'}),201,{'Location':request.url})
+
+    def put_response(self):        
+        return make_response(jsonify({'message':'Replace succesfully'}),200,{'Location':request.url})
+
+    def delete_response(self):
+        return make_response(jsonify({ 'message':'Delete succesfully'}), 202)
+        
 
     def expand_arguments(self):
         """
