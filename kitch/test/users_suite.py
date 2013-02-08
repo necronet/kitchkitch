1db0a3fbe41eb392bdc16a762f22e1885c1ca3b4
Jose Ayerdis <jose.ayerdis@booking.com>
2013-02-08 19:26:23 +0100

    Document the user_test suite

--- kitch/test/users_suite.py
+++ kitch/test/users_suite.py
@@ -80,7 +80,11 @@ class UserTest(BaseTest):
         rv=login(self.c,user['username'],"admin")
         assert rv.status_code == 200
 
-    def test_delete_menus(self):
+    def test_delete_users(self):
+        """
+            Get all users and delete them. Making sure that does not delete the administrative profile.
+            Assert that in the response is a 202 Accepted.
+        """
         items=self.test_get()
 
         for data in items:
@@ -90,33 +94,63 @@ class UserTest(BaseTest):
             assert rv.status_code==202
 
     def test_post_conflict(self):
+        '''
+            Test the creation of a user that already exist in the system.
+        '''
         data=json.dumps({'username':'admin','password':'something','pincode':'0000'})
         rv=self.post(data=data)        
         assert rv.status_code == 409
     
 
 class LoginTest(unittest.TestCase):
+    """
+        Test login resources in general. This is a very special resources that will return
+        authentication token to make calls that required system authorization.
 
+        Note: also note that this Test does not inherit from BaseTest since it will not need for all the 
+        inherit method from it.
+    """
     def setUp(self):
+        """
+            Set up configuration for the test create an test client and 
+            configure the application in TESTING mode.
+        """
         self.c= app.test_client()
         app.config['TESTING'] = True
 
 
     def test_login_wrong_pasword(self):
+        """
+            Try to login using a wrong password.
+            Assert that the response is a 401.
+        """
         rv=login(self.c,"admin","wrongpassword")
         assert rv.status_code == 401
 
     def test_login_right_password(self):
+        """
+        Try to login using a correct password. The response should be 200.
+        """
         rv=login(self.c,"admin","admin")
         assert rv.status_code == 200
 
     def test_logout(self):
+        """
+        Try to logout in order to do this there should be a login and a authtoken to logout.
+        no need to pass any special data in the body.
+
+        Assert that the response is a 202.
+        """
         rv=login(self.c,"admin","admin")
         self.auth_token=json.loads(rv.data)['token']
         rv=self.c.delete('/login/%s'% self.auth_token,content_type='application/json',headers=self.config_headers([]))
         assert rv.status_code == 202
 
     def test_empty_user_and_password(self):
+        """
+            Test an empty username and passwod for three cases and assert
+            400 Bad request for each case.
+        """
         rv=login(self.c,"","admin")
         assert rv.status_code == 400
         rv=login(self.c,"admin","")
@@ -125,6 +159,11 @@ class LoginTest(unittest.TestCase):
         assert rv.status_code == 400
 
     def test_get_login(self):
+        """
+            Test getting the login records, since this is an unimplementend resource, should return
+            501 not implemented. 
+            
+        """
         rv=self.c.get('/login/', content_type='application/json',headers=[('Accept','application/json')])
         assert rv.status_code==501
 
