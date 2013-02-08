42dbfcb8536c594eb232e4c315e739266897a221
Jose Ayerdis <jose.ayerdis@booking.com>
2013-02-08 12:27:39 +0100

    Documenting user_suite testing method

--- kitch/test/users_suite.py
+++ kitch/test/users_suite.py
@@ -9,32 +9,60 @@ import json
 import random
 
 class UserTest(BaseTest):
+    """
+        Test method related to users. Inherit from BaseTest so there are some default 
+        testing behaviour.
+    """
 
     def setUp(self):
         super(UserTest,self).setUp('/user/',auth=True)
 
     def test_get(self):
-        rv=self.get()        
+        """
+            Test the GET method and ensure a 200 OK response 
+            and href self reference is there.
+        """
+        rv=self.get()
         assert rv.status_code == 200
         response_data = json.loads(rv.data)['items']
-        assert len(response_data)        
+        assert len(response_data)>0
         for item in response_data:
             assert item['href'] is not None
 
         return response_data
+
     def test_get_item(self):
+        """
+            Test the retribution of a single user using inherit method
+            check_item, will pass a object so that it can be created 
+            and then retrieve it single instance using GET
+
+            Checks that the retrieve response conains uid, username, pincode.
+        """
         response_object=self.check_item({
                                         'username':'necronet%d' % random.randint(0,500000000),
                                         'password':'necronet',
                                         'pincode':'0000'},['uid','username','pincode'])
 
     def test_post(self):
+        """
+            Create a random user with password necronet and pincode '0000'
+            Ensure that the response is 201.
+        """
         #it is likely that this might failed 1 in 50 million that why such a big number
         data=json.dumps({'username':'necronet%d' % random.randint(0,500000000),'password':'necronet','pincode':'0000'})
         rv=self.post(data=data)        
         assert rv.status_code == 201
 
     def test_put(self):
+        """
+            Test the update of a User resource by 
+            retrieving items from it and picking a random user. 
+            and then updating it's pincode and password.
+            
+            - Assert the response is 200 
+            - that the usermay login with it's new credential
+        """
         items=self.test_get()
         assert len(items) > 0
 
