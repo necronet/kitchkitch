262e797474067d825467dd259f60a69fb61720e3
Jose Ayerdis <jose.ayerdis@booking.com>
2013-02-20 17:04:43 +0100

    add update_object method to TableService to support PUT update method.

--- kitch/orders/views.py
+++ kitch/orders/views.py
@@ -17,5 +17,9 @@ class TableService(BaseService):
     def object_from_json(self, uid, json):
         return [Table(uid, json['name'])]
 
+    def update_object(self, json):
+        table = Table.query.filter_by(active=1, uid=json['uid']).first()
+        table.name = json['name']
+
 app = Blueprint('table',__name__,template_folder='templates')
 register_api(app,TableService, 'tableService','/table/','uid')
