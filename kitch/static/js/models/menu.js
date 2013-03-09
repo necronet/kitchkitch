97937d31de712d367967c121a819170ec95620e1
Jose Ayerdis <jose.ayerdis@booking.com>
2013-03-09 19:19:32 +0100

    Add MenuItem, defaults and validate attributes.

--- kitch/static/js/models/menu.js
+++ kitch/static/js/models/menu.js
@@ -1,5 +1,35 @@
 ( function(){
 
-	Kitch.Models.Menu = Backbone.Model.extend({});
+	Kitch.Models.Menu = Backbone.Model.extend({
+
+		defaults:{
+			items: []
+		},
+		validate: function(attrs, options){
+			if(attrs.title == undefined) {
+				return 'Title must be defined';
+			}
+		}
+	});
+
+	Kitch.Models.MenuItem = Backbone.Model.extend({
+
+		defaults:{
+			addon: false
+		},
+		validate: function(attrs, options){
+			if(attrs.title == undefined) {
+				return 'Title must be defined';
+			}
+
+			if (attrs.description == undefined) {
+				return 'Description must be defined';
+			}
+			
+			if (attrs.price == undefined) {
+				return 'Price must be defined';
+			}	
+		}
+	});
 
 })();
