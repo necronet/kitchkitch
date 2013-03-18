5779ab7d794b07a0ba9ec1b602f305ac8e1191af
Jose Ayerdis <jose.ayerdis@booking.com>
2013-03-18 01:50:45 +0100

    Add routeer to render more views

--- kitch/static/js/routers/router.js
+++ kitch/static/js/routers/router.js
@@ -2,11 +2,20 @@
 	Kitch.Workspace = Backbone.Router.extend({
 		routes: {
 			'': 'home',
-			'logout':'logout'
+			'logout':'logout',
+			'account':'account',
+			'menu':'menu'
+		},
+		menu: function(){
+			console.log('menu');
+			Kitch.Views.App.render(new Kitch.Views.MenuList());
+		},
+		account: function(){
+			Kitch.Views.App.render(new Kitch.Views.Account());
 		},
 
 		home: function(){
-			
+			Kitch.Views.App.render(new Kitch.Views.MenuList());
 		}
 	});
 
