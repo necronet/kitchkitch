ca54e8df0c1bba05d50563123f6f3c429024bb78
Jose Ayerdis <jose.ayerdis@booking.com>
2013-03-18 01:51:16 +0100

    Add account view file

---
+++ kitch/static/js/views/account.js
@@ -0,0 +1,16 @@
+(function(){
+	Kitch.Views.Account = Backbone.View.extend({
+
+		initialize: function(){
+			console.log('initializing');
+		},
+
+		render: function(){
+			console.log('rendering');
+			return this;
+		}
+
+
+	})
+
+})();
