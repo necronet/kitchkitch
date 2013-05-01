a9fe55e6e697e152959764a7ef78457a9648c558
Jose Ayerdis <jose.ayerdis@booking.com>
2013-05-01 22:48:41 +0200

    ADd itsdangerous library

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='Kitch', version='1.0',
       url='http://www.python.org/sigs/distutils-sig/',
       #  Uncomment one or more lines below in the install_requires section
       #  for the specific client drivers/modules your application needs.
-      install_requires=['jinja2','MySQL-Python','werkzeug','flask==0.9','Flask-Login','Flask-SQLAlchemy'],
+      install_requires=['itsdangerous', 'jinja2','MySQL-Python','werkzeug','flask==0.9','Flask-Login','Flask-SQLAlchemy'],
      )
 
 
